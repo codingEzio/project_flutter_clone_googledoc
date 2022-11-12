4d38f06921118816a635de67929720389e93a71f
!Mensch <assassinste@gmail.com>
2022-11-12 19:46:30 +0800

    config: Conenct to the MongoDB on the cloud

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT | 3001;
 
 const app = express();
+const db = `mongodb+srv://app-flutter-googledoc-clone:${process.env.DB_MONGODB_PASSWORD}@cluster0.bnb6949.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('Connected to MongoDB on the cloud');
+  })
+  .catch(err => {
+    console.warn(err);
+  });
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Connected at port ${PORT}`);
