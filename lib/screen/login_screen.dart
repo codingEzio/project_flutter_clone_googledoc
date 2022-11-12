62fffc1db88bb5d16c008f5b0690c0c950d003af
!Mensch <assassinste@gmail.com>
2022-11-12 12:17:26 +0800

    feat: Login button with text on it

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -5,6 +5,19 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+      child: ElevatedButton.icon(
+        onPressed: () {},
+        icon: Image.asset(
+          'assets/images/g-logo-2.png',
+          height: 25,
+        ),
+        label: const Text('Sign in with Google'),
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(150, 50),
+        ),
+      ),
+    ));
   }
 }
