67e0c9eaef95333a64ca6fe4188ff423c8bf5f91
!Mensch <assassinste@gmail.com>
2022-11-12 17:48:13 +0800

    feat: Integrate Google OAuth sign-in

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_flutter_clone_googledoc/colors.dart';
+import 'package:project_flutter_clone_googledoc/repository/auth_repository.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         body: Center(
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () => signInWithGoogle(ref),
         icon: Image.asset(
           'assets/images/g-logo-2.png',
           height: 25,
