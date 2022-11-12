f2bdb4e5d022454c5ed37ece4ec2e2a7b4073014
!Mensch <assassinste@gmail.com>
2022-11-12 12:36:05 +0800

    dev: Configure color for the sign-in button

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_flutter_clone_googledoc/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -15,6 +16,8 @@ class LoginScreen extends StatelessWidget {
         ),
         label: const Text('Sign in with Google'),
         style: ElevatedButton.styleFrom(
+          primary: kWhiteColor,
+          onPrimary: kBlackColor,
           minimumSize: const Size(150, 50),
         ),
       ),
