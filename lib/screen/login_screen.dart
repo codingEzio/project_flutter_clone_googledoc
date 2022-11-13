89e40089927d8b133a3ba441731a476edfbdff41
!Mensch <assassinste@gmail.com>
2022-11-13 09:50:40 +0800

    dev: Check if authenticated successfully as expected

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -2,31 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_flutter_clone_googledoc/colors.dart';
 import 'package:project_flutter_clone_googledoc/repository/auth_repository.dart';
+import 'package:project_flutter_clone_googledoc/screen/home_screen.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
-  void signInWithGoogle(WidgetRef ref) {
-    ref.read(authRepositoryProvider).signInWithGoogle();
+  void signInWithGoogle(WidgetRef ref, BuildContext context) async {
+    final errorModel =
+        await ref.read(authRepositoryProvider).signInWithGoogle();
+
+    if (errorModel.error == null) {
+      ref.read(userProvider.notifier).update((state) => errorModel.data);
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(errorModel.error!),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-        body: Center(
-      child: ElevatedButton.icon(
-        onPressed: () => signInWithGoogle(ref),
-        icon: Image.asset(
-          'assets/images/g-logo-2.png',
-          height: 25,
-        ),
-        label: const Text('Sign in with Google'),
-        style: ElevatedButton.styleFrom(
-          primary: kWhiteColor,
-          onPrimary: kBlackColor,
-          minimumSize: const Size(150, 50),
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () => signInWithGoogle(ref, context),
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 25,
+          ),
+          label: const Text('Sign in with Google'),
+          style: ElevatedButton.styleFrom(
+            primary: kWhiteColor,
+            onPrimary: kBlackColor,
+            minimumSize: const Size(150, 50),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
