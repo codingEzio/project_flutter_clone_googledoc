bbe3a0016b80c79d648143e8c15351f7e5190935
!Mensch <assassinste@gmail.com>
2022-11-13 19:01:27 +0800

    dev: Generate JWT token with user ID and secret key

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const authRouter = express.Router();
@@ -27,8 +30,19 @@ authRouter.post('/api/signup', async (req, res) => {
       user = await user.save();
     }
 
+    // As long as the program could proceed to this line, it surely
+    // already has the OAuth information in hand. So now we need to
+    // generate the JWT token for future authentication of our own,
+    // which is totally based on the public info got from OAuth plus
+    // a token generated from the process. The most common methods
+    // would be used would be .sign() (think of it as attaching our
+    // own data like the user id then plus a secret key to generate
+    // the token) and .verify() (to ensure it is indeed a JWT token
+    // that could identify a person like an user).
+    const token = jwt.sign({ id: user._id }, process.env.AUTH_JWT_SECRET_KEY);
+
     // Return the User info received in the form of JSON
-    res.status(200).json({ user: user });
+    res.status(200).json({ user: user, token: token });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
